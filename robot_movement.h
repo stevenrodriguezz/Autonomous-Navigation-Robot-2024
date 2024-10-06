13a196a1de771fdf9b692a0fc1a4c64c5eca8c07
Steven Rodriguez <72773005+stevenrodriguezz@users.noreply.github.com>
2024-10-06 15:34:54 -0400

    Update robot_movement.h

--- robot_movement.h
+++ robot_movement.h
@@ -13,41 +13,6 @@ pinMode(IN3, OUTPUT);
 pinMode(IN4, OUTPUT);
 }
 
-// void hbridge_loop() {
-// // Rotate the Motor A clockwise
-// digitalWrite(IN1, HIGH);
-// digitalWrite(IN2, LOW);
-// delay(2000);
-// // Motor A
-// digitalWrite(IN1, HIGH);
-// digitalWrite(IN2, HIGH);
-// delay(500);
-// // Rotate the Motor B clockwise
-// digitalWrite(IN3, HIGH);
-// digitalWrite(IN4, LOW);
-// delay(2000);
-// // Motor B
-// digitalWrite(IN3, HIGH);
-// digitalWrite(IN4, HIGH);
-// delay(500);
-// // Rotates the Motor A counter-clockwise
-// digitalWrite(IN1, LOW);
-// digitalWrite(IN2, HIGH);
-// delay(2000);
-// // Motor A
-// digitalWrite(IN1, HIGH);
-// digitalWrite(IN2, HIGH);
-// delay(500);
-// // Rotates the Motor B counter-clockwise
-// digitalWrite(IN3, LOW);
-// digitalWrite(IN4, HIGH);
-// delay(2000);
-// // Motor B
-// digitalWrite(IN3, HIGH);
-// digitalWrite(IN4, HIGH);
-// delay(500);
-// }
-
 void move_forward() {
 
 // Move  Motors CCW - Forward
